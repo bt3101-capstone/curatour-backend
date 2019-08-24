85a5489901d58d8d3e4f7fab595ae4c7c17775f9
Terence Lim <terencelimxp@gmail.com>
2019-08-24 18:01:23 +0800

    add utils

---
+++ src/services/utils.ts
@@ -0,0 +1,29 @@
+import { Response } from 'express';
+/**
+ * Helper function to format Response
+ * @param res 
+ * @param status 
+ * @param message 
+ * @param data 
+ * @param additionalMetadata 
+ */
+export const sendJsonResponse = function(res: Response, status: number, message: string, data = {}, additionalMetadata = {}) {
+    let result;
+    if (data !== {}) {
+        result = data['data'];
+    }
+    const jsonResponse: any = {
+        data: {
+            result
+        },
+        metadata: {
+            status,
+            message,
+        },
+    };
+    Object.assign(jsonResponse.metadata, additionalMetadata);
+
+    return res
+        .status(status)
+        .send(jsonResponse);
+};
