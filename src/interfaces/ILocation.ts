13ef3d779d2bbee6f4e5c53a9c8308d1f771a448
Terence Lim <terencelimxp@gmail.com>
2019-09-02 21:51:07 +0800

    update location data mapping

--- src/interfaces/ILocation.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface ILocation extends Document {
-    country: string;
-    city: string;
-    placesOfInterest: Array<Array<string|number|number>>;
-}
