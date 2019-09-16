0c55c207092adac28d23eba915f7c8494514882f
Terence Lim <terencelimxp@gmail.com>
2019-09-16 15:59:36 +0800

    update blogpost schema

--- src/services/Blog/BlogPostSchema.ts
+++ src/services/Blog/BlogPostSchema.ts
@@ -20,8 +20,71 @@ const BlogPost = new mongoose.Schema(
 const Blog = new mongoose.Schema(
     {
         blogUrl: String,
-        blogTrafficCount: Number,
-        blogs: [{ type: BlogPost }],
+        blogTraffic: [
+            {
+                historicalData: [{
+                    date: String,
+                    pageViews: {
+                        perMillion: Number,
+                        perUser: Number
+                    },
+                    rank: Number,
+                    reach: {
+                        perMillion: Number
+                    }
+                }],
+                usageData: [{
+                    timeRange: {
+                        period: String,
+                        count: Number
+                    },
+                    rank: {
+                        value: Number,
+                        delta: Number
+                    },
+                    reach: {
+                        rank: {
+                            value: Number,
+                            delta: Number
+                        },
+                        perMillion: {
+                            value: Number,
+                            delta: String
+                        }
+                    },
+                    pageViews: {
+                        rank: {
+                            value: Number,
+                            delta: Number
+                        },
+                        perMillion: {
+                            value: Number,
+                            delta: String
+                        },
+                        perUser: {
+                            value: Number,
+                            delta: String
+                        }
+                    }
+                }],
+                contentData: [{
+                    speed: {
+                        medianLoadTime: Number,
+                        percentile: Number
+                    }
+                }]
+            }
+        ],
+        blogs: [{
+            blogUrl: String,
+            blogPost: String,
+            countries: [{ type: String }],
+            cities: [{ type: String }],
+            placesOfInterest: [{ type: String }],
+            spacyNer: [{ startIdx: Number, endIdx: Number, entity: String }],
+            awsNer: [{ type: String }],
+            writtenAt: String
+        }],
         createdAt: String,
         updatedAt: String
     }, {
